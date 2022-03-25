9a09929d4bd3978b95b6957907c67055cd27078e
Nagasaimanoj8 <nagasaimanoj333@gmail.com>
2022-03-26 00:45:11 +0530

    Start

---
+++ StartingPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadderProject
+{
+    internal class StartingPlayer
+    {
+        static void Main(string[] args)
+        {
+            //Variable
+            int currentPosition = 0;
+            Console.WriteLine("Welcome to Snake and Ladder Problem!");
+            Console.WriteLine("Starting position is : " + currentPosition);
+        }
+    }
+}
